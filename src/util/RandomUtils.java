cb03747e3fde5b7a8acdbcfca5e8491589cfbb3d
aducret <aslducret@gmail.com>
2016-06-17 15:58:28 -0300

    Added RandomUtils

---
+++ src/util/RandomUtils.java
@@ -0,0 +1,13 @@
+package util;
+
+public class RandomUtils {
+	
+	public static double randomBetween(double a, double b) {
+		return Math.random() * (b - a) + a;
+	}
+	
+	public static int randomBetween(int a, int b) {
+		return (int)(Math.random() * (b - a + 1) + a);
+	}
+	
+}
