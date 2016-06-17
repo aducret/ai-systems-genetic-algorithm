743d14bd5e0fe2a96ed08657d76ab3a09ec18d19
Franco Prudhomme <francoprud@gmail.com>
2016-06-17 18:07:39 -0300

    Adds probability to crossover implementations

--- src/algorithm/crossover/AnularCrossOver.java
+++ src/algorithm/crossover/AnularCrossOver.java
@@ -6,11 +6,20 @@ import algorithm.model.Chromosome;
 import algorithm.util.ChromosomeUtils;
 
 public class AnularCrossOver implements CrossOverAlgorithm {
+	private double pc;
+	
+	public AnularCrossOver(double pc) {
+		this.pc = pc;
+	}
+	
 	@Override
 	public Pair<Chromosome, Chromosome> crossOver(
 			Pair<Chromosome, Chromosome> pair) {
 		Chromosome firstChild = pair.first.clone();
 		Chromosome secondChild = pair.second.clone();
+	
+		if (!RandomUtils.should(pc))
+			return new Pair<>(firstChild, secondChild);
 		
 		int amountOfGenes = firstChild.geneAmount();
 		int r1 = RandomUtils.randomBetween(0, amountOfGenes);
--- src/algorithm/crossover/UniformCrossOver.java
+++ src/algorithm/crossover/UniformCrossOver.java
@@ -6,9 +6,11 @@ import algorithm.model.Chromosome;
 import algorithm.util.ChromosomeUtils;
 
 public class UniformCrossOver implements CrossOverAlgorithm {
+	private double pc;
 	private double probability;
 
-	public UniformCrossOver(double probability) {
+	public UniformCrossOver(double pc, double probability) {
+		this.pc = pc;
 		this.probability = probability;
 	}
 
@@ -17,6 +19,9 @@ public class UniformCrossOver implements CrossOverAlgorithm {
 			Pair<Chromosome, Chromosome> pair) {
 		Chromosome firstChild = pair.first.clone();
 		Chromosome secondChild = pair.second.clone();
+		
+		if (!RandomUtils.should(pc))
+			return new Pair<>(firstChild, secondChild);
 
 		for (int i = 0; i < firstChild.geneAmount(); i++) {
 			if (RandomUtils.should(probability))
