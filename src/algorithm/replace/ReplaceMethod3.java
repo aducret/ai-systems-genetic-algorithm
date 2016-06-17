c09b7722b7a753ba3deaaeef569800df70fe1c71
aducret <aslducret@gmail.com>
2016-06-17 19:31:34 -0300

    Implement ReplaceMethod3

--- src/algorithm/replace/ReplaceMethod3.java
+++ src/algorithm/replace/ReplaceMethod3.java
@@ -1,15 +1,16 @@
 package algorithm.replace;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import algorithm.model.Chromosome;
 import algorithm.selector.Selector;
 
-public class ReplaceMethod2 implements ReplaceAlgorithm {
-	
+public class ReplaceMethod3 implements ReplaceAlgorithm {
+
 	private Selector selector;
 	
-	public ReplaceMethod2(Selector selector) {
+	public ReplaceMethod3(Selector selector) {
 		this.selector = selector;
 	}
 	
@@ -23,7 +24,10 @@ public class ReplaceMethod2 implements ReplaceAlgorithm {
 		int N = currentGeneration.size();
 		int k = selectedParents.size();
 		List<Chromosome> newGeneration = selector.select(currentGeneration, N-k);
-		newGeneration.addAll(childs);
+		List<Chromosome> currentGenerationAndChilds = new ArrayList<Chromosome>();
+		currentGenerationAndChilds.addAll(currentGeneration);
+		currentGenerationAndChilds.addAll(childs);
+		newGeneration.addAll(selector.select(currentGenerationAndChilds, k));
 		
 		if (newGeneration.size() != N) {
 			System.err.println("The new generation has to have the same size of the old generation!");
