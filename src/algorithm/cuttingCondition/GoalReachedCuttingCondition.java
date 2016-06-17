c7fdcdd945b92d4473cb4777be319778225ee635
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-17 19:15:12 -0300

    goal reached cutting condition

---
+++ src/algorithm/cuttingCondition/GoalReachedCuttingCondition.java
@@ -0,0 +1,31 @@
+package algorithm.cuttingCondition;
+
+import java.util.List;
+
+import algorithm.model.Chromosome;
+
+public class GoalReachedCuttingCondition implements CuttingCondition {
+
+	private double goal;
+	private boolean goalReached;
+
+	public GoalReachedCuttingCondition(double goal) {
+		this.goal = goal;
+	}
+
+	@Override
+	public void onNewGenerationReached(int newGeneration, List<Chromosome> generation, Chromosome bestChromosome) {
+		if (bestChromosome.fitness() >= goal)
+			goalReached = true;
+	}
+
+	@Override
+	public void onBestChromosomeUpdated(Chromosome bestChromosome) {
+
+	}
+
+	@Override
+	public boolean isCutting() {
+		return goalReached;
+	}
+}
