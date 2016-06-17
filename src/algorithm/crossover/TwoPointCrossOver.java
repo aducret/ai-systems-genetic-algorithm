d82873e12914bc9314163890447040ca02dd6bc6
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-17 18:07:22 -0300

    two point cross over fix

--- src/algorithm/crossover/TwoPointCrossOver.java
+++ src/algorithm/crossover/TwoPointCrossOver.java
@@ -6,19 +6,24 @@ import model.Pair;
 import util.RandomUtils;
 
 public class TwoPointCrossOver implements CrossOverAlgorithm {
+	
+	private double pc;
+	
+	public TwoPointCrossOver(double pc) {
+		this.pc = pc;
+	}
 
 	@Override
 	public Pair<Chromosome, Chromosome> crossOver(Pair<Chromosome, Chromosome> pair) {
+		Pair<Chromosome, Chromosome> childs = new Pair<>(pair.first.clone(), pair.second.clone());
+		if (!RandomUtils.should(pc)) return childs;
+		
 		int geneAmount = pair.first.geneAmount();
 		int a = RandomUtils.randomBetween(0, geneAmount - 1);
-		int b = a;
-		while (b == a) {
-			b = RandomUtils.randomBetween(0, geneAmount - 1);
-		}
+		int b = RandomUtils.randomBetween(0, geneAmount - 1);;
 		int r1 = Math.min(a, b);
 		int r2 = Math.max(a, b);
 		
-		Pair<Chromosome, Chromosome> childs = new Pair<>(pair.first.clone(), pair.second.clone());
 		ChromosomeUtils.exchange(childs.first, childs.second, r1, r2);
 		return childs;
 	}
