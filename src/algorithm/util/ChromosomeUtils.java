bf2d996f7d5e4dc2436d665c0f784259412d5794
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-17 17:26:58 -0300

    extended chromosome utils

--- src/algorithm/util/ChromosomeUtils.java
+++ src/algorithm/util/ChromosomeUtils.java
@@ -1,5 +1,7 @@
 package algorithm.util;
 
+import java.util.List;
+
 import algorithm.model.Chromosome;
 import algorithm.model.Gene;
 
@@ -14,5 +16,32 @@ public class ChromosomeUtils {
 		Gene gene = c1.geneAt(r);
 		c1.setGene(r, c2.geneAt(r));
 		c2.setGene(r, gene);
+	}
+	
+	public static double[] relativeFitnesses(List<Chromosome> chromosomes) {
+		double totalFitness = totalFitness(chromosomes);
+		double[] relativeFitnesses = new double[chromosomes.size()];
+		for (int i = 0; i < relativeFitnesses.length; i++) {
+			relativeFitnesses[i] = chromosomes.get(i).fitness() / totalFitness;
+		}
+		return relativeFitnesses;
+	}
+
+	public static double[] cumulativeRelativeFitnesses(double[] relativeFitnesses) {
+		double[] cumulativeRelativeFitnesses = new double[relativeFitnesses.length];
+		cumulativeRelativeFitnesses[0] = relativeFitnesses[0];
+		for (int i = 1; i < cumulativeRelativeFitnesses.length; i++) {
+			cumulativeRelativeFitnesses[i] = cumulativeRelativeFitnesses[i - 1] + relativeFitnesses[i];
+		}
+		return cumulativeRelativeFitnesses;
+	}
+
+	public static double totalFitness(List<Chromosome> chromosomes) {
+		double totalFitness = 0;
+		for (Chromosome chromosome : chromosomes) {
+			totalFitness += chromosome.fitness();
+		}
+		return totalFitness;
+
 	}
 }
