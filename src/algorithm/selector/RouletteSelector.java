a66e1e199a0d71f0c0118cdc39fc1db6c79f0480
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-17 17:43:09 -0300

    roulette selector

---
+++ src/algorithm/selector/RouletteSelector.java
@@ -0,0 +1,23 @@
+package algorithm.selector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import algorithm.model.Chromosome;
+import algorithm.util.ChromosomeUtils;
+
+public class RouletteSelector implements Selector {
+
+	@Override
+	public List<Chromosome> select(List<Chromosome> chromosomes, int k) {
+		List<Chromosome> selected = new ArrayList<>();
+		double[] relativeFitnesses = ChromosomeUtils.relativeFitnesses(chromosomes);
+		double[] cumulativeRelativeFitnesses = ChromosomeUtils.cumulativeRelativeFitnesses(relativeFitnesses);
+		for (int i = 0; i < k; i++) {
+			double number = Math.random();
+			int winnerIndex = ChromosomeUtils.getWinner(cumulativeRelativeFitnesses, number);
+			selected.add(chromosomes.get(winnerIndex));
+		}
+ 		return selected;
+	}
+}
