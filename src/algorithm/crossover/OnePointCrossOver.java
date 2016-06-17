dc291478ba9bb0bb58dc89ffb7163a9c2662933e
aducret <aslducret@gmail.com>
2016-06-17 18:41:32 -0300

    Refactor OnePointCrossOver

--- src/algorithm/crossover/OnePointCrossOver.java
+++ src/algorithm/crossover/OnePointCrossOver.java
@@ -3,26 +3,28 @@ package algorithm.crossover;
 import util.RandomUtils;
 import model.Pair;
 import algorithm.model.Chromosome;
-import algorithm.model.Gene;
+import algorithm.util.ChromosomeUtils;
 
 public class OnePointCrossOver implements CrossOverAlgorithm {
-
+	
+	private double pc;
+	
+	public OnePointCrossOver(double pc) {
+		this.pc = pc;
+	}
+	
 	@Override
 	public Pair<Chromosome, Chromosome> crossOver(Pair<Chromosome, Chromosome> pair) {
-		Chromosome child1 = pair.first.clone();
-		Chromosome child2 = pair.second.clone();
-		int amountOfGenes = child1.geneAmount();
-		int locus = RandomUtils.randomBetween(1, amountOfGenes);
-		int index = locus - 1;
-
-		for(int i = index; i < amountOfGenes; i++) {
-			Gene gene1 = child1.geneAt(i);
-			Gene gene2 = child2.geneAt(i);
-			child1.setGene(i, gene2);
-			child2.setGene(i, gene1);
-		}
+		Pair<Chromosome, Chromosome> childs = new Pair<Chromosome, Chromosome>(pair.first.clone(), pair.second.clone());
+		if (!RandomUtils.should(pc)) { return childs; }
 		
-		return new Pair<Chromosome, Chromosome>(child1, child2);
+		int amountOfGenes = pair.first.geneAmount();
+		int locus = RandomUtils.randomBetween(1, amountOfGenes);
+		int from = locus - 1;
+		int to = childs.first.geneAmount()-1;
+		ChromosomeUtils.exchange(childs.first, childs.second, from, to);
+		
+		return childs;
 	}
 	
 }
