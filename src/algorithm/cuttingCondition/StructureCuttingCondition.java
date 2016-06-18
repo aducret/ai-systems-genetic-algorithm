1b3fd8d9de2004811102a6c1d9b1af3872c63636
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-17 21:17:55 -0300

    structure cutting condition

---
+++ src/algorithm/cuttingCondition/StructureCuttingCondition.java
@@ -0,0 +1,50 @@
+package algorithm.cuttingCondition;
+
+import java.util.HashSet;
+import java.util.List;
+
+import algorithm.model.Chromosome;
+
+/*
+ * if tolerance percent of the population or more remains the same from
+ * generation to generation, isCutting will return true
+ * */
+public class StructureCuttingCondition implements CuttingCondition {
+
+	private double tolerance;
+	private boolean structureRemainsTheSame;
+	private HashSet<Chromosome> previousGeneration;
+
+	public StructureCuttingCondition(double tolerance) {
+		this.tolerance = tolerance;
+		previousGeneration = new HashSet<>();
+	}
+
+	@Override
+	public void onNewGenerationReached(int newGeneration, List<Chromosome> generation, Chromosome bestChromosome) {
+		double remainedTheSame = changePercentage(generation);
+		if (remainedTheSame > tolerance) {
+			structureRemainsTheSame = true;
+		} else {
+			previousGeneration = new HashSet<>(generation);
+		}
+	}
+
+	@Override
+	public void onBestChromosomeUpdated(Chromosome bestChromosome) {
+	}
+
+	@Override
+	public boolean isCutting() {
+		return structureRemainsTheSame;
+	}
+
+	private double changePercentage(List<Chromosome> currentGeneration) {
+		int occurrences = 0;
+		for (Chromosome chromosome : currentGeneration) {
+			if (previousGeneration.contains(chromosome))
+				occurrences++;
+		}
+		return occurrences / ((double) currentGeneration.size());
+	}
+}
