a627bbd22305d61f4d49dd8e93e2f3f073df7d01
goodengineer <gutierrez_nacho@hotmail.com>
2016-06-18 14:18:09 -0300

    selector pairing algorithm implemented

---
+++ src/algorithm/pairing/SelectorPairingAlgorithm.java
@@ -0,0 +1,24 @@
+package algorithm.pairing;
+
+import java.util.List;
+
+import algorithm.chromosome.Chromosome;
+import algorithm.selector.Selector;
+
+public class SelectorPairingAlgorithm implements PairingAlgorithm {
+
+	private Selector selector;
+
+	public SelectorPairingAlgorithm(Selector selector) {
+		this.selector = selector;
+	}
+
+	@Override
+	public void makePairs(List<Chromosome> chromosomes, int k, Callback cb) {
+		for (int i = 0; i < k; i++) {
+			List<Chromosome> selected = selector.select(chromosomes, 2);
+			cb.onPairAvailable(new Pair<>(selected.get(0), selected.get(1)));
+		}
+	}
+
+}
